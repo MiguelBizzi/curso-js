1c74d1e193be64ba96aa66c0efb6440fae7c1720
Bizzi <106081382+MiguelBizzi@users.noreply.github.com>
2022-08-15 01:41:44 -0300

    Add files via upload

---
+++ Exercicios/ExercicioTabuada/script.js
@@ -0,0 +1,16 @@
+
+y = 1;
+x = 1;
+
+while (x <= 10) {
+    y = 1;
+    while (y <= 10) {
+    console.log(x + ' x ' + y + ' = ' + (x * y )+ "<br>");
+    document.write(x + ' x ' + y + ' = ' + x * y +"<br>");
+    y++;
+    }
+    document.write("<hr/>");
+    x++;
+    }
+
+
